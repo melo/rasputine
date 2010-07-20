e205e9d51d3648734d2ed5196de267fea9a51693
Pedro Melo <melo@simplicidade.org>
2010-07-20 15:51:06 +0100

    Rename Plugin AsciiBin to MooAsciiBin
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- App-Rasputine/lib/App/Rasputine/Plugins/MooAsciiBin.pm
+++ App-Rasputine/lib/App/Rasputine/Plugins/MooAsciiBin.pm
@@ -5,12 +5,13 @@ use warnings;
 use base 'App::Rasputine::Plugin';
 use Encode qw( encode decode );
 
+
 sub to_world {
   my ($self, $raw) = @_;
-  
+
   my $bytes = encode('latin1', decode('utf8', $raw));
   $bytes =~ s/([\x80-\xff])/sprintf('~%X', ord($1))/ge;
-  
+
   return $bytes;
 }
 
